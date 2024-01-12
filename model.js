51785a31d4dd527408222205253cc2572483fd9d
KuzAnton8335 <ant2783@gmail.com>
2024-01-12 15:56:01 +0300

    new blocks

--- model.js
+++ model.js
@@ -1,21 +1,64 @@
-import image from "./public/assests/image.png";
+import { Block } from "./classes/blocks.js";
+import image from "/assests/image.png";
+
+const text = `Крутые уроки по Javascript тут:`;
 
 export const model = [
-	{
-		type: "title", value: "Конструктор сайтов на чистом Javascript", options: {
-			tag: 'h2',
-			styles: {
-				background: "linear-gradient(to right,#ff0099,#493240)",
-				color: "#fff",
-				padding: "1.5rem",
-				'text-align': "center",
-			},
-		}
-	},
-	{
-		type: "text",
-		value: "here we go with some text",
-	},
-	{ type: "columns", value: ["1111111", "2222222", "3333333", "44444444"] },
-	{ type: "image", value: image },
+//   new Block(type:"title"),
+  {
+    type: "title",
+    value: "Конструктор сайтов на чистом Javascript",
+    options: {
+      tag: "h2",
+      styles: {
+        background: "linear-gradient(to right,#ff0099,#493240)",
+        color: "#fff",
+        padding: "1.5rem",
+        "text-align": "center",
+      },
+    },
+  },
+  {
+    type: "image",
+    value: image,
+    options: {
+      styles: {
+        padding: "2rem 0",
+        display: "flex",
+        "justify-content": "center",
+      },
+      imageStyles: {
+        width: "500px",
+        height: "auto",
+      },
+      alt: "картинка",
+    },
+  },
+  {
+    type: "columns",
+    value: [
+      "Приложение на чистом Javascript, без использования библиотек",
+      "Узнаешь как работает принцип SOLID и ОПП  в Javascript за один курс",
+      "Javascript - это просто,интересно. Научись создавать любые UI своими руками",
+    ],
+    options: {
+      styles: {
+        background: "linear-gradient(to bottom,#8e2de2,#4a00e0)",
+        padding: "2rem",
+        color: "#fff",
+        "font-weight": "bold",
+      },
+    },
+  },
+  {
+    type: "text",
+    value: text,
+    options: {
+      styles: {
+        background: "linear-gradient(to left,#f2994a,#f2c94c)",
+        padding: "1rem",
+        "font-weight": "bold",
+      },
+    },
+  },
 ];
--- templates.js
+++ templates.js
@@ -1,24 +1,28 @@
 import { col, css, row } from "./utils.js";
 
 export function title(block) {
-	const { tag = 'h1', styles } = block.options
-	return row(col(`<${tag}>${block.value}</${tag}>`), css(styles));
+  const { tag = "h1", styles } = block.options;
+  return row(col(`<${tag}>${block.value}</${tag}>`), css(styles));
 }
 
 export function text(block) {
-	return row(col(`<p>${block.value}</p>`));
+  return row(col(`<p>${block.value}</p>`), css(block.options.styles));
 }
 
 export function columns(block) {
-	const html = block.value.map(col).join("");
-	return row(html);
+  const html = block.value.map(col).join("");
+  return row(html, css(block.options.styles));
 }
 export function image(block) {
-	return row(`<img src="${block.value}" />`);
+  const { imageStyles, alt = "", styles } = block.options;
+  return row(
+    `<img src="${block.value} " alt="${alt}" style="${css(imageStyles)}"/>`,
+    css(styles),
+  );
 }
 export const templates = {
-	title: title,
-	text: text,
-	image: image,
-	columns: columns,
-}
+  title: title,
+  text: text,
+  image: image,
+  columns: columns,
+};
